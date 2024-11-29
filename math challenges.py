2ca609b5772a381b9c6918b9e98361ab7b819904
Antonin <antonin.autret@efrei.net>
2024-11-29 20:08:00 +0100

    test2

--- math challenges.py
+++ math challenges.py
@@ -1,10 +1,25 @@
 #file where all the function used for math challenges
 
+
+import random
 #function that takes an integer and return his factorial
 def factorial(n):
     a=1
     for i in range(1,n+1):
         a*=i
     return a
+# function that generate a random integer between 1 and 10 and ask the user the factorial of it(return True if right and False otherwise)
+def math_challenge_factorial():
+    n=random.randint(1,10)
+    print("Calculate the factorial of {}".format(n))
+    ans=int(input("Your answer: "))
+    if ans==factorial(n):
+        print("Correct! You win a key.")
+        return True
+    else:
+        print("Wrong! No key for you.")
+        return False
 
+
+math_challenge_factorial()
 
