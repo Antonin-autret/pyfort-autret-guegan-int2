c2b00c7fb97fb61a0054c98a9cbb3778ba10bad0
Antonin <antonin.autret@efrei.net>
2024-11-29 19:56:27 +0100

    test2

--- math challenges.py
+++ math challenges.py
@@ -1,1 +1,10 @@
+#file where all the function used for math challenges
+
+#function that takes an integer and return his factorial
+def factorial(n):
+    a=1
+    for i in range(1,n+1):
+        a*=i
+    return a
+
 
