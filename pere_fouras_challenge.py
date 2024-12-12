dc1e53d0c3c904cd53d2448642ebdfbbed3471a6
antonin <antonin.autret@efrei.net>
2024-12-12 17:25:34 +0100

    creat the function to load the content of a file

--- pere_fouras_challenge.py
+++ pere_fouras_challenge.py
@@ -1,7 +1,15 @@
 import json
+import random
 #load a json file and return its what is inside
 def load_file(file):
     pf = open(file,"r")
     dico = json.load(pf)
     pf.close()
     return dico
+
+def pere_fouras_riddles():
+    riddle =load_file('data/PFRiddles.json')[random.randint(0,11)]
+    print(riddle['question'])
+    a=input("the ")
+
+pere_fouras_riddles()
